8ea2f2b7d28d954f7cfb4119e7ebf4cd57e6b1cd
lfrdreamman <553350172@qq.com>
2014-12-28 12:12:05 +0800

    kkk

---
+++ src/Document.cpp
@@ -0,0 +1,18 @@
+/*
+ * Document.cpp
+ *
+ *  Created on: 2014年12月28日
+ *      Author: lfr
+ */
+
+#include "Document.h"
+
+Document::Document() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Document::~Document() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Document.h
@@ -0,0 +1,17 @@
+/*
+ * Document.h
+ *
+ *  Created on: 2014年12月28日
+ *      Author: lfr
+ */
+
+#ifndef DOCUMENT_H_
+#define DOCUMENT_H_
+
+class Document {
+public:
+	Document();
+	virtual ~Document();
+};
+
+#endif /* DOCUMENT_H_ */
